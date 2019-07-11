91136bb39b814866edf4af149292b6c1e41adea3
liurui <liu.rui@oyohotels.cn>
2019-07-11 10:19:53 +0800

    上传git仓库

---
+++ boot-user-service-provider/src/main/java/com/atguigu/gmall/bootuserserviceprovider/BootUserServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.bootuserserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUserServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootUserServiceProviderApplication.class, args);
+    }
+
+}
