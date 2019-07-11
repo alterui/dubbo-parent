825460e61bb3ff968f41e1fe08a082c47ded6b7e
liurui <liu.rui@oyohotels.cn>
2019-07-11 10:15:24 +0800

    开始引入springboot

---
+++ boot-order-service-consumer/src/main/java/com/atguigu/gmall/bootorderserviceconsumer/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.bootorderserviceconsumer.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author liurui
+ * @time 2019-07-10 20:38
+ */
+@Controller
+public class OrderController {
+
+}
